1ee3fb2948eba92887f4f28b6c84a7dc6af5be72
Thomsen Cummings <thomsencummings@gmail.com>
2020-10-05 13:00:45 -0400

    fix faulty todays date helper

--- src/helpers/getTodaysDate.js
+++ src/helpers/getTodaysDate.js
@@ -1,9 +1,12 @@
 export default function getTodaysDate() {
   const date = new Date();
   const yyyy = `${date.getFullYear()}`;
-  let mm = `${date.getMonth()}`;
+
+  // NB: Date's "getMonth" function is ZERO INDEXED
+  let mm = `${date.getMonth() + 1}`;
   if (mm.length === 1) mm = "0" + mm;
-  let dd = `${date.getDay()}`;
+
+  let dd = `${date.getDate()}`;
   if (dd.length === 1) dd = "0" + dd;
 
   return `${yyyy}-${mm}-${dd}`;
