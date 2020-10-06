dec9b972d137750a1c1410b40de2a779c7f76d24
Thomsen Cummings <thomsencummings@gmail.com>
2020-10-06 18:05:08 -0400

    add fetch mock to jest setup

---
+++ __mocks__/fetch.js
@@ -0,0 +1,37 @@
+import faker from "faker";
+
+import { makeFakeMaterials } from "../src/helpers/testHelpers";
+import {
+  API_GET,
+  API_POST,
+  API_PATCH,
+  API_DELETE,
+} from "../src/helpers/apiCall";
+
+// Mock the fetch function
+export default function fetch(url, req) {
+  switch (req.method) {
+    case API_GET:
+      const materials = makeFakeMaterials(5);
+
+      return Promise.resolve({
+        json: () => Promise.resolve(materials),
+      });
+
+    case API_POST:
+      const id = faker.random.uuid();
+
+      return Promise.resolve({
+        json: () => Promise.resolve(Object.assign({}, req.body, { id })),
+      });
+
+    case API_PATCH:
+    case API_DELETE:
+      return Promise.resolve({
+        json: () => Promise.resolve(req.body),
+      });
+
+    default:
+      return Promise.reject("Garbage input for fetch call");
+  }
+}
--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,9 @@
 import React from "react";
-
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+
+import fetch from "./__mocks__/fetch";
+
+global.fetch = fetch;
 
 configure({ adapter: new Adapter() });
